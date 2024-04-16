df3a6f7277a4d68c9beb4d212603262cbb08aad2
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-16 04:22:08 +0000

    creating the model

--- football/models.py
+++ football/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Football(models.Model):
+    """
+    Stores a single football related to :model:`auth.User`
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+     author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="soccer_football" 
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
+
+
+    class Meta:
+        """To display the posts by created_on in ascending order"""
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        """Return a string representation of the object (the post's title and author)."""
+        return f"{self.title} | written by {self.author}"
