e6c250b2d3586832c2be8697cc16b575615581bc
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-16 03:08:37 +0000

    readies code for deploy

--- allsoccer/urls.py
+++ allsoccer/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from football.views import football_soccer
 
 urlpatterns = [
+    path('football/', football_soccer, name='football'),
     path('admin/', admin.site.urls),
 ]
--- football/views.py
+++ football/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def football_soccer(request):
+    return HttpResponse("Hello, Blog!")
