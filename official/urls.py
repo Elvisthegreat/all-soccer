dc7ecd4db6b9742cebb5c629e8769911b8c9f504
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-01 16:15:46 +0000

    Add functionality to the eidt and delete button

--- official/urls.py
+++ official/urls.py
@@ -5,4 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('official/', views.OfficialList.as_view(), name='official'),
     path('official_news/<slug:slug>/', views.official_news, name='official_news'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', # The comment form's action attribute points to the URL for the comment_edit view, with the ID of the selected comment
+         views.comment_edit, name='comment_edit'), # urls for editing comment
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),# urls for deleting comment
 ]
--- official/views.py
+++ official/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Official, Comment
 from .forms import CommentForm
 
@@ -27,6 +29,11 @@ def official_news(request, slug):
             comment.author = request.user
             comment.officials = officials
             comment.save()
+            # for successfully message
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted successfully'
+            )
 
     comment_form = CommentForm()
     
@@ -41,4 +48,61 @@ def official_news(request, slug):
             "comment_form": comment_form,
         },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+    """
+    if request.method == "POST":
+
+        queryset = Official.objects.all()
+        officials = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user: # checking aain if the form is valid and if it still same user
+            comment = comment_form.save(commit=False)
+            comment.officials = officials
+            comment.approved = False # Turn the previous approved commemt to unapproved
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('official_news', args=[slug]))
+
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    """
+    queryset = Official.objects.all()
+    officials = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('official_news', args=[slug]))
+
 
