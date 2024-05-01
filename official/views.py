5f1eff66af0d47371277bdb226c9017fe169b2dc
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-01 00:30:48 +0000

    Displaying comment added from the database

--- official/views.py
+++ official/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Official, Comment
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -15,13 +16,29 @@ class OfficialList(generic.ListView):
 def official_news(request, slug):
     queryset = Official.objects.all()
     officials = get_object_or_404(queryset, slug=slug)
+    comments = officials.comments.all().order_by("created_on")
+    comment_count = officials.comments.all().count()
 
+    """Handling the Post request from the comment form"""
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.officials = officials
+            comment.save()
+
+    comment_form = CommentForm()
+    
     return render(
         request,
         "official/official_news.html",
 
         {
             'officials': officials,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
 
