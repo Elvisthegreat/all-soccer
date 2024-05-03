5d02efe390281746598bd2a56746a6fc54f4244f
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-03 16:00:06 +0000

    added and style the succcess message

--- static/js/script.js
+++ static/js/script.js
@@ -57,34 +57,40 @@ deleteButtons.forEach(button => {
 
 
 
+// Vote section
 
-// Initialize counts for each button
-// let countGreen = 0;
-// let countBlue = 0;
-// let countRed = 0;
+// Initialize counts for each button or load them from localStorage
+let countGreen = localStorage.getItem('countGreen') ? parseInt(localStorage.getItem('countGreen')) : 0;
+let countBlue = localStorage.getItem('countBlue') ? parseInt(localStorage.getItem('countBlue')) : 0;
+let countRed = localStorage.getItem('countRed') ? parseInt(localStorage.getItem('countRed')) : 0;
 
-// // Define functions to increment each count and update the respective button content
-// function incrementCountGreen() {
-//     countGreen += 1;
-//     let button1 = document.getElementById('div-green-color');
-//     button1.innerHTML = `${countGreen} %<br><span>Home</span>`;
-// }
+// Update button content with the current count from localStorage
+document.getElementById('div-green-colorBarca').innerHTML = `${countGreen} %<br><span>Home</span>`;
+document.getElementById('div-blue-colorDBP').innerHTML = `${countBlue} %<br><span>Draw</span>`;
+document.getElementById('div-red-colorPsg').innerHTML = `${countRed} %<br><span>Away</span>`;
 
-// function incrementCountBlue() {
-//     countBlue += 1;
-//     let button2 = document.getElementById('div-blue-color');
-//     button2.innerHTML = `${countBlue} %<br><span>Draw</span>`;
-// }
+// Define functions to increment each count, update the respective button content, and save to localStorage
+function incrementCountGreen() {
+    countGreen += 1;
+    localStorage.setItem('countGreen', countGreen);
+    document.getElementById('div-green-colorBarca').innerHTML = `${countGreen} %<br><span>Home</span>`;
+}
 
-// function incrementCountRed() {
-//     countRed += 1;
-//     let button3 = document.getElementById('div-red-color');
-//     button3.innerHTML = `${countRed} %<br><span>Away</span>`;
-// }
+function incrementCountBlue() {
+    countBlue += 1;
+    localStorage.setItem('countBlue', countBlue);
+    document.getElementById('div-blue-colorDBP').innerHTML = `${countBlue} %<br><span>Draw</span>`;
+}
 
-// // Attach event listeners to each button for the respective function
-// document.getElementById('div-green-color').addEventListener('click', incrementCountGreen);
-// document.getElementById('div-blue-color').addEventListener('click', incrementCountBlue);
-// document.getElementById('div-red-color').addEventListener('click', incrementCountRed);
+function incrementCountRed() {
+    countRed += 1;
+    localStorage.setItem('countRed', countRed);
+    document.getElementById('div-red-colorPsg').innerHTML = `${countRed} %<br><span>Away</span>`;
+}
+
+// Attach event listeners to each button for the respective function
+document.getElementById('div-green-colorBarca').addEventListener('click', incrementCountGreen);
+document.getElementById('div-blue-colorDBP').addEventListener('click', incrementCountBlue);
+document.getElementById('div-red-colorPsg').addEventListener('click', incrementCountRed);
 
 
