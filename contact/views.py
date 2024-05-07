6033b95a6a309f628d52e29c9b5727bfcd007897
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-07 03:54:27 +0000

    Reduce crispy form width and height

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from django.contrib import messages
 from .models import ContactUs
